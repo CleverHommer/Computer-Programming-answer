78d821a4f3e1807cca774bda44915ef0f43de3b7
tc162 <2506081199@qq.com>
2025-10-31 03:22:33 +0800

    master

--- 第五次作业/P157_17.c
+++ 第五次作业/P157_17.c
@@ -1,16 +1,21 @@
 //思路：输入字符串后利用strlen函数求字符串长度，从头到中间逐个将字符与其对称位置的字符交换后输出
 #include<stdio.h>
 #include<string.h>//需要使用这个库里头的strlen函数
+void reverse(int len,char* str){
+    for(int i=0;2*i<len-1;i++){
+        char tmp=0;
+        tmp=str[i];
+        str[i]=str[len-1-i];
+        str[len-1-i]=tmp;
+    }
+    return;
+}
 int main(){
     int n=0;
-    char tmp=0,str[105]={0};//定义数组时预留5格空位，保证不会出现越界问题
+    char str[105]={0};//定义数组时预留5格空位，保证不会出现越界问题
     scanf("%s",str);
     n=strlen(str);
-    for(int i=0;2*i<n-1;i++){
-        tmp=str[i];
-        str[i]=str[n-1-i];
-        str[n-1-i]=tmp;
-    }
+    reverse(n,str);
     puts(str);
     return 0;
 }
