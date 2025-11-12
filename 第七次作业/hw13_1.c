2c608656973b3216f944cf89d2bb4eb05d496e1a
tc162 <2506081199@qq.com>
2025-11-12 15:14:04 +0800

    第七次作业答案

--- 第七次作业/hw13_1.c
+++ 第七次作业/hw13_1.c
@@ -1,14 +1,21 @@
-//思路：设置一个指针从字符串开头一直往后检查，直到遇到第一个'\0'停下，计算指针往后移动了几个字节（每个char变量都占用1字节）
+//思路：设置一个指针从字符串开头一直往后检查，遇到第一个'\0'停下，计算指针往后移动了几位
 #include<stdio.h>
 int _strlen(char* str){
     char *tail=str;
-    while(*tail!='\0') tail++;//有一个非常简洁的写法：while(*++tail);（前提是字符串非空）
-    return tail-str;//这个程序在处理长度超过105的字符串时可能会死循环，最好还是在while循环里面加入break语句
+    while(tail-str<101){
+        if(*tail=='\0') return tail-str;
+        tail++;
+    }
+    return -1;
 }
 int main(){
     char str[105]={0};
     scanf("%s",str);
     int len=_strlen(str);
+    if(len==-1){
+        puts("超出处理能力！");
+        return -1;
+    }
     printf("%d",len);
     return 0;
 }
